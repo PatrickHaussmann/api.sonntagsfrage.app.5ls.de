75f4ce0d858996a7ad07fad1cf0da8df241e4547
Patrick Haußmann <git@patrickhaussmann.de>
2021-04-26 17:56:22 +0000

    return name and color of parties

--- api/history.js
+++ api/history.js
@@ -12,24 +12,50 @@ module.exports = async (req, res) => {
     let el_thead = el_table.getElementsByTagName("thead")[0];
     let el_tbody = el_table.getElementsByTagName("tbody")[0];
 
-    let indices = {
-        CDU: 2,
-        SPD: 3,
-        GRÜNE: 4,
-        FDP: 5,
-        LINKE: 6,
-        AfD: 7,
-        Sonstige: 8,
-    }
-
-
     function parse(text) {
         return Number(text.trim().replace("-", "").replace(" ", "").replace("%", "").replace(",", "."))
     }
 
-    /* parties: Object.keys(indices), */
+    // colors from https://de.wikipedia.org/wiki/Vorlage:Wahldiagramm/Partei/DE
     let result = {
-        history: [],
+        parties: {
+            CDU: {
+                index: 2,
+                name: "Christlich Demokratische Union Deutschlands",
+                color: "#000000"
+            },
+            SPD: {
+                index: 3,
+                name: "Sozialdemokratische Partei Deutschlands",
+                color: "#E3000F"
+            },
+            GRÜNE: {
+                index: 4,
+                name: "BÜNDNIS 90/DIE GRÜNEN",
+                color: "#46962b"
+            },
+            FDP: {
+                index: 5,
+                name: "Freie Demokratische Partei",
+                color: "#ffff00"
+            },
+            LINKE: {
+                index: 6,
+                name: "DIE LINKE",
+                color: "#BE3075"
+            },
+            AfD: {
+                index: 7,
+                name: "Alternative für Deutschland",
+                color: "#009ee0"
+            },
+            Sonstige: {
+                index: 8,
+                name: "Sonstige",
+                color: "#cccccc"
+            },
+        },
+        history: []
     }
     for (let i = 0; i < el_tbody.children.length; i++) {
         const el_tr = el_tbody.children[i];
@@ -39,15 +65,21 @@ module.exports = async (req, res) => {
             date: el_tr.children[0].innerHTML,
             Befragte: parse(el_tr.children[10].innerHTML.replace(".", "")),
         };
-        for (const party in indices) {
-            if (Object.hasOwnProperty.call(indices, party)) {
-                const index = indices[party];
+        for (const party in result.parties) {
+            if (Object.hasOwnProperty.call(result.parties, party)) {
+                const index = result.parties[party].index;
                 row.parties[party] = parse(el_tr.children[index].innerHTML);
             }
         }
 
         result.history.push(row);
     };
+
+    for (const party in result.parties) {
+        if (Object.hasOwnProperty.call(result.parties, party)) {
+            result.parties[party].index = undefined;
+        }
+    }
 
     res.json(result)
 }
